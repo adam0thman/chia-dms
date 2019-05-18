eff3ed0f19e18c84bf41073d21406e4e453f3603
Nik M Adam Hafiz Nik Othman <adam0thman@gmail.com>
2019-05-18 08:24:18 +0800

    addition of pages

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -1,35 +1,36 @@
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDXvI4NJFwWTP5rHFCxY_WGPna3cKIVmV8",
-    authDomain: "morhabshi-e9680.firebaseapp.com",
-    databaseURL: "https://morhabshi-e9680.firebaseio.com",
-    projectId: "morhabshi-e9680",
-    storageBucket: "morhabshi-e9680.appspot.com",
-    messagingSenderId: "704004043273"
-  };
-  
-  //Initialize main firebase app
-  firebase.initializeApp(config);
-  
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
-    
-  // Variables
-  var pageURL = window.location.protocol+"//"+window.location.host;
-  var pageLogin = "/index.html";
-  var pageMain = "/main.html";
-  var pageAdmin = "/admin.html";
-  console.log("pageURL is: "+pageURL);
-  console.log("pageLogin is: "+pageLogin);
-  console.log("pageMain is: "+pageMain);
-  console.log("pageAdmin is: "+pageAdmin);
 
-  // Function for Logging out
-  function callLogout() {
-    firebase.auth().signOut().then(function() {
-      console.log("User has been Signed Out!");
-      window.location.replace(pageURL+pageLogin);
-    }).catch(function(e) {
-      console.log("Error signing out:",e);
-    });
-  }
+var firebaseConfig = {
+  apiKey: "AIzaSyB-euI19G3u4FuTD1FpFOyPjyjhJMiOf8k",
+  authDomain: "dms-chia.firebaseapp.com",
+  databaseURL: "https://dms-chia.firebaseio.com",
+  projectId: "dms-chia",
+  storageBucket: "dms-chia.appspot.com",
+  messagingSenderId: "132326518160",
+  appId: "1:132326518160:web:09502dab93c4ae57"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+// Variables
+var pageURL = window.location.protocol + "//" + window.location.host;
+var pageLogin = "/index.html";
+var pageMain = "/main.html";
+var pageAdmin = "/admin.html";
+console.log("pageURL is: " + pageURL);
+console.log("pageLogin is: " + pageLogin);
+console.log("pageMain is: " + pageMain);
+console.log("pageAdmin is: " + pageAdmin);
+
+// Function for Logging out
+function callLogout() {
+  firebase.auth().signOut().then(function () {
+    console.log("User has been Signed Out!");
+    window.location.replace(pageURL + pageLogin);
+  }).catch(function (e) {
+    console.log("Error signing out:", e);
+  });
+}
